cc3aaaf7cad279782df8288a501961cf8c00c9b2
Sujan Poudel <sujanp@skybasestudios.com.np>
2023-12-29 15:07:52 +1100

    rename css class

--- src/pages/Blog/partials/AdditionalBlog.js
+++ src/pages/Blog/partials/AdditionalBlog.js
@@ -1,5 +1,7 @@
-import Blogcover1 from "../../../assets/images/blog4.jpg";
-import Blogcover2 from "../../../assets/images/blog5.png";
+import Blogthumb1 from "../../../assets/images/blog4.jpg";
+import Blogthumb2 from "../../../assets/images/blog5.png";
+import Blogthumb3 from "../../../assets/images/blog6.png";
+
 
 import { Link } from "react-router-dom";
 export default function AdditionalBlog() {
@@ -7,11 +9,11 @@ export default function AdditionalBlog() {
     <div className="col-sm-12 col-md-12 col-lg-4 mb-3 py-4">
       <div className="ps-0 ps-lg-5">
         <h2 className="section-heading fs-5">Explore More</h2>
-        <div className="blog-card-cover">
-          <div className="blog-cover-img">
-            <img src={Blogcover1} alt="" className="img-fluid" />
+        <div className="blog-card-thumb">
+          <div className="blog-thumb-img">
+            <img src={Blogthumb1} alt="" className="img-fluid" />
           </div>
-          <div className="blog-cover-desc">
+          <div className="blog-thumb-desc">
             <Link to="/blog/detail">
               <h5>Securing Backend Services Of Application</h5>
             </Link>
@@ -21,11 +23,11 @@ export default function AdditionalBlog() {
             </div>
           </div>
         </div>
-        <div className="blog-card-cover">
-          <div className="blog-cover-img">
-            <img src={Blogcover2} alt="" className="img-fluid" />
+        <div className="blog-card-thumb">
+          <div className="blog-thumb-img">
+            <img src={Blogthumb2} alt="" className="img-fluid" />
           </div>
-          <div className="blog-cover-desc">
+          <div className="blog-thumb-desc">
             <Link to="/blog/detail">
               <h5>Where to focus UI VS UX ?</h5>
             </Link>
@@ -33,6 +35,20 @@ export default function AdditionalBlog() {
               <a href={() => false}>#UX Research</a>
               <a href={() => false}>#UI</a>
               <a href={() => false}>#Design</a>
+            </div>
+          </div>
+        </div>
+        <div className="blog-card-thumb">
+          <div className="blog-thumb-img">
+            <img src={Blogthumb3} alt="" className="img-fluid" />
+          </div>
+          <div className="blog-thumb-desc">
+            <Link to="/blog/detail">
+              <h5>Making your ecommerce stand out</h5>
+            </Link>
+            <div className="blog-tags mt-3">
+              <a href={() => false}>#ecommerce</a>
+              <a href={() => false}>#business</a>
             </div>
           </div>
         </div>
